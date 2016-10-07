40d5faaad221c2dd1a6b23c6662fb09ae023a22e
johnnyGrow <John.Grow@MU-C1-15.mullen.pvt.k12.co.us>
2016-10-07 09:26:41 -0600

    little changes

--- Inheritance/src/IndividualSport.java
+++ Inheritance/src/IndividualSport.java
@@ -1,5 +1,10 @@
 
 public class IndividualSport extends AthleticActivities
 	{
-
+		public IndividualSport(String n,String d,String r)
+			{
+				name=n;
+				description=d;
+				rules=r;
+			}
 	}
--- Inheritance/src/Runner.java
+++ Inheritance/src/Runner.java
@@ -7,7 +7,7 @@ public class Runner
 			{
 				ArrayList<Entertainment>myList=new ArrayList<Entertainment>();
 				myList.add(new Sport("Lacrosse","a sport made the native americans to entertain God", "cannot touch ball with hands and no unneccessary roughness"));
-				myList.add(new Sport("Tennis","h","h"));
+				myList.add(new ReadingBook("The Hobbit","a children's fantasy novel by English author J. R. R. Tolkien. It was published on 21 September 1937 to wide critical acclaim, being nominated for the Carnegie Medal and awarded many prizes","the story of Bilbo Baggins, a hobbit who lives in Hobbiton. He enjoys a peaceful and pastoral life but his life is interrupted by a surprise visit by the wizard Gandolf, who guides the hobbit on his journey to saving the world", ""));
 			//	myList.add(new Sports"");
 
 			}
